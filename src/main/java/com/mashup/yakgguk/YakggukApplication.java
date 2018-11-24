6480275871aa89a4afad814c02537ba1616d8759
KiWooyoung <ueong777@gmail.com>
2018-11-24 20:18:21 +0900

    first-commit

---
+++ src/main/java/com/mashup/yakgguk/YakggukApplication.java
@@ -0,0 +1,12 @@
+package com.mashup.yakgguk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakggukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YakggukApplication.class, args);
+    }
+}
