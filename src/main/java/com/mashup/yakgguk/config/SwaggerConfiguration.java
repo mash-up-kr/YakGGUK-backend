528b5bbba53b045448357d47f5763b3fdd616d98
KiWooyoung <ueong777@gmail.com>
2018-11-24 21:27:01 +0900

    test commit

--- src/main/java/com/mashup/yakgguk/config/SwaggerConfiguration.java
+++ src/main/java/com/mashup/yakgguk/config/SwaggerConfiguration.java
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-
+test
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
