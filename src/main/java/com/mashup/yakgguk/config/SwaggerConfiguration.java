9e4bdf34fc9b530e1ba2b6fb79b6a41091a9e5ba
sdh2398 <ehdgus2398@naver.com>
2018-12-22 16:44:09 +0900

    필요없는 문자 삭제

--- src/main/java/com/mashup/yakgguk/config/SwaggerConfiguration.java
+++ src/main/java/com/mashup/yakgguk/config/SwaggerConfiguration.java
@@ -19,7 +19,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-test
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
